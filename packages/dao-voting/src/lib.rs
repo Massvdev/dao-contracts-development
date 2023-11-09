77972d2e3fa96b768c3ee357435f0ad5b77b62c4
Massvdev <roadlizardinc@gmail.com>
2023-11-09 09:41:23 -0600

    Update deprecated function names to_binary and from_binary #761

---
+++ packages/dao-voting/src/lib.rs
@@ -0,0 +1,12 @@
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+
+pub mod deposit;
+pub mod duration;
+pub mod error;
+pub mod multiple_choice;
+pub mod pre_propose;
+pub mod proposal;
+pub mod reply;
+pub mod status;
+pub mod threshold;
+pub mod voting;
